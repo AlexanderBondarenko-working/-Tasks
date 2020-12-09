e883b0dcb9caf781ea06d0b49aa14b3e93b24f6e
Alexander Bondarenko <AlexanderBondarenko@coherentsolutions.com>
2020-12-09 18:02:22 +0300

    added default constructor

---
+++ task1/task1/basematrix.cpp
@@ -0,0 +1,18 @@
+#include "basematrix.h"
+
+BaseMatrix::BaseMatrix() : numberOflines(0), numberOfcolums(0), matrix(nullptr) {}
+
+BaseMatrix::BaseMatrix(const int numberOflines, const int numberOfcolums,
+    const int* fillingArray, const int sizeOfarray)
+{
+}
+
+BaseMatrix::BaseMatrix(const BaseMatrix& source)
+{
+   
+}
+
+
+BaseMatrix::~BaseMatrix() {
+   
+}
