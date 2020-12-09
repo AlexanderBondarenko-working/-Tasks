a2734f33f8c4ba1100729e141e9e4dbd0939b7fc
Alexander Bondarenko <AlexanderBondarenko@coherentsolutions.com>
2020-12-09 18:04:50 +0300

    added constructor with parameters

--- task1/task1/basematrix.cpp
+++ task1/task1/basematrix.cpp
@@ -4,7 +4,12 @@ BaseMatrix::BaseMatrix() : numberOflines(0), numberOfcolums(0), matrix(nullptr) 
 
 BaseMatrix::BaseMatrix(const int numberOflines, const int numberOfcolums,
     const int* fillingArray, const int sizeOfarray)
+    : numberOflines(numberOflines), numberOfcolums(numberOfcolums)
 {
+    matrix = new int* [numberOflines];
+    for (int index = 0; index < numberOflines; ++index) {
+        matrix[index] = new int[numberOfcolums];
+    }
 }
 
 BaseMatrix::BaseMatrix(const BaseMatrix& source)
@@ -13,6 +18,7 @@ BaseMatrix::BaseMatrix(const BaseMatrix& source)
 }
 
 
+
 BaseMatrix::~BaseMatrix() {
-   
+    
 }
