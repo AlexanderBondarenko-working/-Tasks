76e1217e7e1a34e8ae4c8f859993ab539e775343
Alexander Bondarenko <AlexanderBondarenko@coherentsolutions.com>
2020-12-10 11:32:51 +0300

    added constructor with parameters

--- task1/task1/diagonalmatrix.cpp
+++ task1/task1/diagonalmatrix.cpp
@@ -7,9 +7,17 @@ DiagonalMatrix::DiagonalMatrix(const int numberOflines, const int numberOfcolums
     : BaseMatrix(numberOflines, numberOfcolums), diagonalMatrix(nullptr) {}
 
 DiagonalMatrix::DiagonalMatrix(const int numberOflines, const int numberOfcolums, const int*
-	fillingArray, const int sizeOfarray) 
+	fillingArray, const int sizeOfarray) : BaseMatrix(numberOflines, numberOfcolums)
 {
+	if (numberOfcolums != sizeOfarray) {
+		throw std::invalid_argument("invalid array length");
+	}
 
+	diagonalMatrix = new int[sizeOfarray];
+
+	for (int index = 0; index < sizeOfarray; ++index) {
+		diagonalMatrix[index] = fillingArray[index];
+	}
 }
 
 DiagonalMatrix::DiagonalMatrix(const DiagonalMatrix& source) 
