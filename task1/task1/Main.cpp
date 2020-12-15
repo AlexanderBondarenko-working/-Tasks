ea04170865c98cadbc6cdcece2e721c1f3e40915
Alexander Bondarenko <AlexanderBondarenko@coherentsolutions.com>
2020-12-15 12:49:51 +0300

    added unique_ptr in Main

--- task1/task1/Main.cpp
+++ task1/task1/Main.cpp
@@ -5,20 +5,23 @@ using namespace std;
 
 int main() {
 	int fillingArray[28] { 1, 2, 3, 4, 5, 6, 7, 1, 2, 3, 4, 5, 6, 1, 2, 3, 1, 2, 3, 4, 5, 6, 7, 1, 2, 3, 6, 6 };
-	int forNullMatrix[28]{ 1, 2, 3, 4, 5, 6, 7, 1, 2, 3, 4, 5, 6, 1, 2, 3, 1, 2, 3, 4, 5, 6, 7, 1, 2, 3, 6, 6 };
+	int forSecondMatrix[28]{ 1, 2, 3, 4, 5, 6, 7, 1, 2, 3, 4, 5, 6, 1, 2, 3, 1, 2, 3, 4, 5, 6, 7, 1, 2, 3, 6, 6 };
 	int forDiagonalMatrix[4]{ 1, 2, 3, 4 };
 	int forDiagonalMatrix2[7]{ 1, 2, 3, 4, 5, 6, 7};
 	try
 	{
-		BaseMatrix<int> firstBaseMatr(4, 7, fillingArray, 28);
-		cout << firstBaseMatr.matrixToString() << "\n";
-		BaseMatrix<int> secondBaseMatr(7, 4, forNullMatrix, 28);
-		cout << secondBaseMatr.matrixToString() << "\n";
-		DiagonalMatrix<int> diagMatr(7, 4, forDiagonalMatrix, 4);
-		DiagonalMatrix<int> diagMatr2(4, 7, forDiagonalMatrix2, 7);
-		cout << diagMatr.matrixToString() << "\n";
-		cout << diagMatr2.matrixToString() << "\n";
-		cout << (diagMatr * diagMatr2) -> matrixToString() << "\n";
+		std::unique_ptr<BaseMatrix<int>> firstBaseMatr(new BaseMatrix<int>(4, 7, fillingArray, 28));
+		std::unique_ptr<BaseMatrix<int>> secondBaseMatr(new BaseMatrix<int>(7, 4, forSecondMatrix, 28));
+		cout << firstBaseMatr -> matrixToString() << "\n";
+		cout << secondBaseMatr -> matrixToString() << "\n";
+		
+		std::unique_ptr <DiagonalMatrix<int>> firstDiagMatr(new DiagonalMatrix<int>(7, 4, forDiagonalMatrix, 4));
+		std::unique_ptr <DiagonalMatrix<int>> secondDiagMatr(new DiagonalMatrix<int>(4, 7, forDiagonalMatrix2, 7));
+		cout << firstDiagMatr -> matrixToString() << "\n";
+		cout << secondDiagMatr -> matrixToString() << "\n";
+		std::unique_ptr<BaseMatrix<int>> resultBaseMatr ((*firstBaseMatr) * (*secondBaseMatr));
+		//resultBaseMatr = ((*firstBaseMatr) * (*secondBaseMatr));
+		cout << resultBaseMatr -> matrixToString() << "\n";
 		//cout << (diagMatr * firstBaseMatr)->matrixToString() << "\n";
 		//cout << result.matrixToString() << "\n";
 		//cout << DiagonalMatrix<int>(7, 4).matrixToString();
