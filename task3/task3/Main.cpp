9012d8026ab256bd3a4ceea7d7671aa23c4d09fa
Alexander Bondarenko <AlexanderBondarenko@coherentsolutions.com>
2020-12-29 17:50:56 +0300

    added some chenges

--- task3/task3/Main.cpp
+++ task3/task3/Main.cpp
@@ -14,40 +14,29 @@
 
 int main() {
 
-	//Person p;
-	//
+	json j;
+	j["name"] = "Ivan";
+	j["surname"] = "Ivanov";
+	j["age"] = 32;
+	j["adr"] = { {"city", "Minsk"}, {"streetAddress", "Pushkina, 11"} };
+	
+	std::vector<Person> persons;
+	persons.resize(3);
+	persons.at(0) = j.get<Person>();
+	j["name"] = "Misha";
+	persons.at(1) = j.get<Person>();
+	j["name"] = "Maksim";
+	persons.at(2) = j.get<Person>();
 
-	//json j;
-	////Person p2;
-	//std::vector<Person> persons;
+	json j_vec(persons);
 
-	//j["name"] = "Ivan";
-	//j["surname"] = "Ivanov";
-	//j["age"] = 32;
-	//j["adr"] = { {"city", "Minsk"}, {"streetAddress", "Pushkina, 11"} };
-	//std::cout << j << std::endl << std::endl;
-
-	//persons.resize(3);
-	//persons.at(0) = j.get<Person>();
-	//j["name"] = "Misha";
-	//persons.at(1) = j.get<Person>();
-	//j["name"] = "Maksim";
-	//persons.at(2) = j.get<Person>();
-
-
-	//json j_vec(persons);
-	//std::cout << j_vec.size() << "  " << j.size() << std::endl;
-	//std::cout << "jvec_obj " << j_vec.is_object() << " " << "jvec_arr " << j_vec.is_array() << " j_obj " << j.is_object() << " j_arr " << j.is_array() << std::endl;
 	try {
 		std::ifstream inpt("file.json");
 		if (!inpt) {
-			json j;
-			std::string str;
-			std::getline(inpt, str);
-			std::cout << str;
+			
 			//inpt >> j;
-			std::cout << j << std::endl;
-			std::unique_ptr<std::vector<Person>> persons = PersonBuilder::getObject().createPersons(j, BaseChecker());
+			//std::cout << j << std::endl << std::endl;
+			std::unique_ptr<std::vector<Person>> persons = PersonBuilder::getObject().createPersons(j_vec, BaseChecker());
 
 			json j2;
 			for (std::vector<Person>::iterator it = persons->begin(); it != persons->end(); ++it) {
